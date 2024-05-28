d85f70dc7df0826d5ae748be13b94333dac7f6b2
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2024-05-28 20:11:05 +0300

    add typehints

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from itertools import chain
 from pathlib import Path
+from typing import Set, List, Optional
 import json
 import click
 
@@ -8,31 +9,27 @@ flatten = chain.from_iterable
 
 
 class Vault:
-    def __init__(self, filename):
+    def __init__(self, filename: str):
         self.filename = filename
 
         with open(filename, "r") as f:
             converted_sets = {k: set(v) for k, v in json.load(f).items()}
             self.data = defaultdict(set, converted_sets)
 
-    def files(self, tags=None):
-        """Lists files, by filtering by tags.
-
-        Type of tags is list[set].
-        """
+    def files(self, tags: Optional[List[Set[str]]] = None) -> List[str]:
         return [
             file
             for file, f_tags in self.data.items()
             if any(t.issubset(f_tags) for t in tags) or not tags
         ]
 
-    def tags(self):
+    def tags(self) -> List[str]:
         return sorted(set(flatten(self.data.values())))
 
-    def add_tags(self, file, tags):
+    def add_tags(self, file: str, tags: Set[str]):
         self.data[file] |= tags
 
-    def remove_tags(self, file, tags):
+    def remove_tags(self, file: str, tags: Set[str]):
         self.data[file] -= tags
 
     def __enter__(self):
@@ -83,13 +80,13 @@ class DelimitedSet(click.ParamType):
 @click.group()
 @click.option("--vault", type=click.Path(), default="./vault.json")
 @click.pass_context
-def cli(ctx, vault):
+def cli(ctx, vault: Path):
     ctx.obj = ctx.with_resource(Vault(vault))
 
 
 @cli.command()
 @click.argument("vaultname")
-def init_vault(vaultname):
+def init_vault(vaultname: str):
     Vault.init(vaultname)
 
 
@@ -98,7 +95,7 @@ def init_vault(vaultname):
 @click.option("-t", "tags", type=DelimitedSet())
 @click.option("-f", "filename", type=click.Path(exists=True), multiple=True)
 @click.option("-r", "read", type=click.File("r"), help="Read file or stdin (-r -)")
-def add_tag(vault: Vault, filename, tags, read):
+def add_tag(vault: Vault, filename: List[Path], tags: Set[str], read: str):
     filenames = filename or []
     if read:
         filenames.extend(read.read().strip().split("\n"))
@@ -111,7 +108,7 @@ def add_tag(vault: Vault, filename, tags, read):
 @click.pass_obj
 @click.option("-t", "tags", type=DelimitedSet())
 @click.option("-f", "filename", type=click.Path(exists=True), multiple=True)
-def remove_tag(vault: Vault, filename, tags):
+def remove_tag(vault: Vault, filename: List[Path], tags: Set[str]):
     for fn in filename:
         vault.remove_tags(fn, tags)
 
@@ -125,14 +122,14 @@ def remove_tag(vault: Vault, filename, tags):
     multiple=True,
     help="Each instance of -t is considered an AND condition, which is then OR'd with others",
 )
-def ls(vault, tags):
+def ls(vault: Vault, tags: List[Set[str]]):
     for file in vault.files(tags):
         click.echo(file)
 
 
 @cli.command()
 @click.pass_obj
-def list_tags(vault):
+def list_tags(vault: Vault):
     for tag in vault.tags():
         click.echo(tag)
 
