d4b9703e06beac4542b71a3673799f7e4d23fa8d
Simo Hellgrén <hellgrensimo@gmail.com>
2026-01-23 21:11:14 +0200

    move things around

--- filetags/cli.py
+++ filetags/cli.py
@@ -1,42 +1,15 @@
 from pathlib import Path
-from sqlite3 import Connection
 
 import click
 
 from filetags import service
 from filetags.commands import db, tag, tagalong
-from filetags.db.connect import get_vault
+from filetags.commands.context import LazyVault
 from filetags.models.node import Node
 from filetags.parser import parse
 from filetags.utils import compile_pattern, flatten
 
 DEFAULT_VAULT_PATH = Path("./vault.db")
-
-
-class LazyVault:
-    """Lazily gets vault only when it is truly accessed."""
-
-    def __init__(self, path: Path, ctx: click.Context):
-        self._path = path
-        self._ctx = ctx
-        self._conn: Connection | None = None
-
-    def _get_conn(self) -> Connection:
-        if self._conn is None:
-            if not self._path.exists():
-                raise click.ClickException(
-                    f"{self._path} does not exist. Run `ftag db init {self._path}` to create"
-                )
-
-            self._conn = self._ctx.with_resource(get_vault(self._path))
-
-        return self._conn
-
-    def __enter__(self) -> Connection:
-        return self._get_conn().__enter__()
-
-    def __exit__(self, *args):
-        return self._conn.__exit__(*args)
 
 
 @click.group()
